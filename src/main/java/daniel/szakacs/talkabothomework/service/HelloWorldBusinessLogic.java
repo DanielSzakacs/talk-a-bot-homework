0aa36dc1492891c66862d1f91a7e1df8bc51ac5f
danielszakacs <daniel.sokach@gmail.com>
2019-04-08 14:27:38 +0200

    Service method 'getText()' is created.

--- src/main/java/daniel/szakacs/talkabothomework/service/HelloWorldBusinessLogic.java
+++ src/main/java/daniel/szakacs/talkabothomework/service/HelloWorldBusinessLogic.java
@@ -1,4 +1,21 @@
 package daniel.szakacs.talkabothomework.service;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import daniel.szakacs.talkabothomework.repository.HelloWorldRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelloWorldBusinessLogic {
+
+    private HelloWorldRepository helloWorldRepository;
+
+    public HelloWorldBusinessLogic(HelloWorldRepository helloWorldRepository) {
+        this.helloWorldRepository = helloWorldRepository;
+    }
+
+    public JSONPObject getText(){
+//        return new JSONPObject("text",this.helloWorldRepository.getById(Long.parseLong("1")));
+        return new JSONPObject("text", "Hello World");
+    }
+
 }
