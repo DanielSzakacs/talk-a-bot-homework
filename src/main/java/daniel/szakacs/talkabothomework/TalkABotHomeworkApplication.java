b525521103325a5108ed89eff7b731bce33045ef
danielszakacs <daniel.sokach@gmail.com>
2019-04-08 14:09:31 +0200

    mane files added to git .

---
+++ src/main/java/daniel/szakacs/talkabothomework/TalkABotHomeworkApplication.java
@@ -0,0 +1,13 @@
+package daniel.szakacs.talkabothomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkABotHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkABotHomeworkApplication.class, args);
+    }
+
+}
